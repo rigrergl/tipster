bf17f0e6224b0e94d346ad30ac8206e80030fb18
Rigre Reinier Garciandia Larquin <rigrergl@fb.com>
2021-06-22 16:50:49 -0500

    added view controller. endEditing on tap

---
+++ Tipster/TipViewController.m
@@ -0,0 +1,37 @@
+//
+//  TipViewController.m
+//  Tipster
+//
+//  Created by Rigre Reinier Garciandia Larquin on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onTap:(id)sender {
+    NSLog(@"hello");
+    
+    [self.view endEditing:true];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
